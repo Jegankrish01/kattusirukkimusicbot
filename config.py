18b5afeee579115fc270e236cfe191d849f0f0da
Jegankrish01 <124910598+Jegankrish01@users.noreply.github.com>
2023-02-11 20:41:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    API_ID = "26254684"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "e170b8c39484cfd9232773cd63e4a5fb" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5598822360:AAEpL9o-LWpgdu4rQBLZ2R5qrwgoh-dTCXw" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
